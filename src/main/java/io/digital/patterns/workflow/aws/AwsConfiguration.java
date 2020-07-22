e8b3d17ad42ee83d1ebf3f9945093f26e08bfac0
Mark Olliver <mark@digitalpatterns.io>
2020-07-22 09:56:48 +0100

    upstream fix

--- src/main/java/io/digital/patterns/workflow/aws/AwsConfiguration.java
+++ src/main/java/io/digital/patterns/workflow/aws/AwsConfiguration.java
@@ -2,10 +2,12 @@ package io.digital.patterns.workflow.aws;
 
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.ClientConfiguration;
+import com.amazonaws.ClientConfigurationFactory;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.retry.PredefinedBackoffStrategies;
+import com.amazonaws.retry.RetryMode;
 import com.amazonaws.retry.RetryPolicy;
 import com.amazonaws.retry.RetryUtils;
 import com.amazonaws.services.s3.AmazonS3;
@@ -35,8 +37,8 @@ public class AwsConfiguration {
     public AWSStaticCredentialsProvider credentials(){
         BasicAWSCredentials basicAWSCredentials =
                 new BasicAWSCredentials(awsProperties.getCredentials().getAccessKey()
-                , awsProperties.getCredentials().getSecretKey());
-       return  new AWSStaticCredentialsProvider(basicAWSCredentials);
+                        , awsProperties.getCredentials().getSecretKey());
+        return  new AWSStaticCredentialsProvider(basicAWSCredentials);
 
     }
 
@@ -65,24 +67,27 @@ public class AwsConfiguration {
                         Math.toIntExact(TimeUnit.SECONDS.toMillis(5))
                 ), 3, true);
 
-        return AmazonS3ClientBuilder
+        AmazonS3ClientBuilder amazonS3ClientBuilder = AmazonS3ClientBuilder
                 .standard()
-                .withClientConfiguration(new ClientConfiguration()
-                .withRetryPolicy(retryPolicy))
+                .withRegion(Regions.fromName(awsProperties.getRegion()))
+                .withCredentials(credentials());
+
+        ClientConfigurationFactory clientConfigurationFactory = new ClientConfigurationFactory();
+        clientConfigurationFactory.getConfig().withRetryPolicy(retryPolicy).withRetryMode(RetryMode.STANDARD);
+        amazonS3ClientBuilder.withClientConfiguration(clientConfigurationFactory.getConfig());
+        return amazonS3ClientBuilder.build();
+    }
+
+    @Bean
+    public AmazonSimpleEmailService amazonSimpleEmailService() {
+        return AmazonSimpleEmailServiceClientBuilder.standard()
                 .withRegion(Regions.fromName(awsProperties.getRegion()))
                 .withCredentials(credentials()).build();
     }
 
     @Bean
-    public AmazonSimpleEmailService amazonSimpleEmailService() {
-       return AmazonSimpleEmailServiceClientBuilder.standard()
-               .withRegion(Regions.fromName(awsProperties.getRegion()))
-               .withCredentials(credentials()).build();
-    }
-
-    @Bean
     public AmazonSNSClient amazonSNSClient() {
-       return (AmazonSNSClient) AmazonSNSClient.builder()
+        return (AmazonSNSClient) AmazonSNSClient.builder()
                 .withRegion(Regions.fromName(awsProperties.getSnsRegion()))
                 .withCredentials(credentials()).build();
 
